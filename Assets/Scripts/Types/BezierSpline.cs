d14a545ce4a3109251abd1bef9f772e67193e180
r.giner <remi.giner@hotmail.fr>
2023-03-07 19:41:41 +0100

    [curve] Fixed t getter from Hermitian curve

--- Assets/Scripts/Types/BezierSpline.cs
+++ Assets/Scripts/Types/BezierSpline.cs
@@ -6,6 +6,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BezierSpline", menuName = "Splines/BezierSpline", order = 1)]
 public class BezierSpline : SplineDescriptor
 {
+    Matrix4x4 M = new Matrix4x4(new Vector4(-1f, 3f,-3f, 1f),
+                                new Vector4( 3f,-6f, 3f, 0f),
+                                new Vector4(-3f, 3f, 0f, 0f),
+                                new Vector4( 1f, 0f, 0f, 0f));
+
+    public override void GetT(float u, int inputCount, out float t, out int startingPoint)
+    {
+        int knotCount = inputCount / 2;
+        float knotQuantity = u * (knotCount - 1);
+        int startingKnot = Mathf.FloorToInt(knotQuantity);
+
+        startingPoint = startingKnot * 2;
+
+        t = knotQuantity - startingKnot;
+    }
+
     public override bool IsPointAKnot(int PointID) => true;
 
     private static float Factorial(int input)
@@ -48,5 +64,34 @@ public class BezierSpline : SplineDescriptor
         }
 
         return Result;
+    }
+
+    public override Vector3 EvaluateFromMatrix(float u, List<Vector3> inputPoints)
+    {
+        GetT(u, inputPoints.Count, out float t, out int startingPoint);
+
+        List<Vector3> intervallePoints = inputPoints.GetRange(startingPoint, 4);
+
+        return GetGeometryMatrix(intervallePoints) * M * GetTimeVector(t);
+    }
+
+    public Vector4 GetTimeVector(float time)
+    {
+        float timeSqr = time * time;
+        float timeCube = timeSqr * time;
+        return new Vector4(timeCube, timeSqr, time, 1f);
+    }
+
+    public Matrix4x4 GetGeometryMatrix(List<Vector3> inputPoints)
+    {
+        Vector3 PointA = inputPoints[0];
+        Vector3 PointB = inputPoints[1];
+        Vector3 PointC = inputPoints[2];
+        Vector3 PointD = inputPoints[3];
+
+        return new Matrix4x4(new Vector4(PointA.x, PointA.y, PointA.z, 0f),
+                             new Vector4(PointB.x, PointB.y, PointB.z, 0f),
+                             new Vector4(PointC.x, PointC.y, PointC.z, 0f),
+                             new Vector4(PointD.x, PointD.y, PointD.z, 1f));
     }
 }
